2b0c2f817099fae3c66e11f6c885c8f3041454fc
hoangthai150199 <hoangthai150199@gmail.com>
2021-11-16 15:48:56 +0700

    Fix bug Ex02 01

--- Ex2/js/script.js
+++ Ex2/js/script.js
@@ -1,30 +1,31 @@
-let selectElement = document.querySelector('#exampleFormControlSelect1');
+const selectElement = document.querySelector('#exampleFormControlSelect1');
 selectElement.onchange = function(event){
     let valueSelected = event.target.value;
     let liElements = document.querySelectorAll('li');
-    //======
+
     for(let i = 0; i < liElements.length; i++) {
         liElements[i].classList.remove('active-item')
     }
-    // =======
-    if(valueSelected === 'even') {
+    if (valueSelected == 'reset' || valueSelected == 'select') {
+        for(let i = 0; i < liElements.length; i++) {
+            liElements[i].classList.remove('active-item')
+        }
+    }
+    else if(valueSelected === 'even') {
         for(let i = 0; i < liElements.length; i++) {
             if(i%2 === 1) {
                 liElements[i].classList.add('active-item')
             }
         }
-    } else if (valueSelected == 'odd') {
+    }
+    else if (valueSelected == 'odd') {
         for(let i = 0; i < liElements.length; i++) {
             if(i%2 === 0) {
                 liElements[i].classList.add('active-item')
             }
         }
-    } else if(valueSelected == 'reset') {
-        for(let i = 0; i < liElements.length; i++) {
-            liElements[i].classList.remove('active-item')
-        }
-    } else {
+    }
+    else {
         liElements[+valueSelected - 1].classList.add('active-item')
     }
 }
-
