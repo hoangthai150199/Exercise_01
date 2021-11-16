c26156939d197305f4f923853c53054c7898bd3e
hoangthai150199 <hoangthai150199@gmail.com>
2021-11-16 15:31:48 +0700

    Fix bug ex6 01

--- Ex6/js/script.js
+++ Ex6/js/script.js
@@ -3,19 +3,17 @@ var personData = [
         name : 'Thai',
         phone : '0342552949',
         email : 'thailch@runsystem.net',
-        action : 'delete'
     },
     {
         name : 'Duy',
         phone : '0123456789',
         email : 'duy@gmail.com',
-        action : 'delete'
     },
 ]
 
 const dataBody = document.querySelector('#data-body')
 
-let initPersonRender = personData.map(person => {
+var initPersonRender = personData.map(person => {
     return `
         <tr>
         <th scope="row">
@@ -24,39 +22,34 @@ let initPersonRender = personData.map(person => {
         <td class="col">${person.name}</td>
         <td class="col">${person.phone}</td>
         <td class="col">${person.email}</td>
-        <td class="col">${person.action}</td>
+        <td class="col"><button type="button" onclick="deleteAction(event)" class="btn btn-danger delete-action">Delete</button></td>
     </tr>`
 })
 
 dataBody.innerHTML = initPersonRender.join('');
 
-var tdsElemt = document.querySelectorAll('td')
-
-tdsElemt.forEach(td => {
-    td.contentEditable  = true;
-})
-
-//delete
-var deleteBtn = document.querySelector('#delete-btn')
-deleteBtn.onclick = function() {
-    let selectedPersons = document.querySelectorAll('th > input')
-    selectedPersons.forEach(e => {
-        if(e.checked === true) {
-            console.log(e.parentElement.parentElement)
-            e.parentElement.parentElement.remove()
-        }
+// Double click to edit
+var addEditEvents = function() {
+    var tds = document.querySelectorAll('#data-body td');
+    tds.forEach(e => {
+        e.spellcheck = false;
+        e.addEventListener('dblclick', function(event) {
+            event.target.contentEditable = true;
+            event.target.focus();
+        })
+        e.addEventListener('focusout', function(event) {
+            event.target.contentEditable = false;
+        })
     })
 }
+addEditEvents();
 
-var checkall = document.querySelector('#check-all')
-checkall.onclick = function () {
-    var checklist = document.querySelectorAll('input')
-    checklist.forEach(input => {
-        input.checked = true;
-    })
+// Button delete on action column
+var deleteAction = function(event) {
+    event.target.parentElement.parentElement.remove();
 }
 
-//add
+// Button Add
 var btnAdd = document.querySelector('#add-btn');
 btnAdd.addEventListener('click', function() {
     var e = document.createElement('tr');
@@ -67,8 +60,28 @@ btnAdd.addEventListener('click', function() {
         <td class="col"></td>
         <td class="col"></td>
         <td class="col"></td>
-        <td class="col"></td>
-    `
-    dataBody.appendChild(e)
-    enableEditor();
-}) 
+        <td class="col"><button type="button" onclick="deleteAction(event)" class="btn btn-danger delete-action">Delete</button></td>
+    `;
+    dataBody.appendChild(e);
+    addEditEvents();
+})
+
+// Checked on all item
+var checkall = document.querySelector('#check-all')
+checkall.onclick = function () {
+    var checklist = document.querySelectorAll('input')
+    checklist.forEach(input => {
+        input.checked = true;
+    })
+}
+
+// Button Delete
+const deleteBtn = document.querySelector('#delete-btn')
+deleteBtn.onclick = function() {
+    let selectedPersons = document.querySelectorAll('th > input')
+    selectedPersons.forEach(e => {
+        if(e.checked === true) {
+            e.parentElement.parentElement.remove()
+        }
+    })
+}
